fea4a36f3df63c7e643a79ac0670aea9465ec78f
Emily Rudling <mmrudling@yahoo.com>
2020-09-21 16:22:13 +0100

    Pokemon object now contains name and attributes

--- src/main/java/training/busboard/Pokemon.java
+++ src/main/java/training/busboard/Pokemon.java
@@ -1,7 +1,10 @@
 package training.busboard;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Pokemon {
     public String name;
+    public List<Object> abilities;
 }
