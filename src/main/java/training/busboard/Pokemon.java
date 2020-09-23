741b213c37b8d7b19d881712ec7d9df2ef23dd45
Emily Rudling <mmrudling@yahoo.com>
2020-09-23 13:05:51 +0100

    Commit before experimenting at lunch

--- src/main/java/training/busboard/Pokemon.java
+++ src/main/java/training/busboard/Pokemon.java
@@ -3,6 +3,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.imageio.plugins.png.PNGImageReader;
 
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -13,8 +15,30 @@ public class Pokemon {
     public int id;
     public LinkedHashMap sprites;
 
+    //this gets the pokemon sprite
     public String getSpriteURL(){
         return(String) sprites.get("front_default");
     }
-   //public String species;
+
+//    //these return the abilities
+//    public Object getAbilities(int i) {
+//        return abilities.get(i);
+//    }
+//    public String getAbility(int i){
+//        return getAbilities(i).get("ability");
+//    }
+//    public String getAbilityName(){
+//        String abilityString = "";
+//        for (int i = 0; i < abilities.size(); i++){
+//            String x = getAbility(i).get("name");
+//            abilityString = abilityString + " " + x;
+//        }
+//        return abilityString;
+//        //this returns the abilities in an easy to read string
+//    }
+
+    //finds the species of the pokemon
+     public String species(){
+          return null;
+     }
 }
